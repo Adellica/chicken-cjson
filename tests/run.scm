1b7202bcabe5a7b5bb54a62487f4a1154ed15482
Kristian Lein-Mathisen <kristianlein@gmail.com>
2015-09-26 18:28:34 +0200

    test: adds more basic testing

--- tests/run.scm
+++ tests/run.scm
@@ -5,4 +5,13 @@
  (test "empty object"  '() (string->json "{}"))
  (test "empty vector" '#() (string->json "[]"))
 
- (test "number" 12.5 (string->json "12.5")))
+ (test "number" 12.5 (string->json "12.5"))
+ (test "false" #f (string->json "false"))
+ (test "true" #t (string->json "true"))
+ (test "null" (void) (string->json "null"))
+
+ (test "nested"
+       '#( ((b . 2.0)
+            (a . 1.0))
+           ((x . -1.0)))
+       (string->json "[{\"a\" : 1 , \"b\" : 2}, {\"x\" : -1 }]")))
